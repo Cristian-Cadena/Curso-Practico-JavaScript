55bf640c663b4489ae3c9c115e24cd12e51392d1
CristianCadena <cristiancadena1927@gmail.com>
2022-02-20 22:35:14 -0500

    Analisis Salario

---
+++ Taller 4/salarios.js
@@ -0,0 +1,121 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+   });
+
+colombia.push({
+   name: "Natalia",
+    salary: 1200,
+   });
+
+colombia.push({
+    name: "Claudia",
+    salary: 2000,
+   });
+
+colombia.push({
+    name: "Lilia",
+    salary: 1500,
+   });
+
+colombia.push({
+   name: "Faber",
+    salary: 1700,
+   });
+
+colombia.push({
+    name: "Mary",
+    salary: 1900,
+   });
+
+colombia.push({
+    name: "Reineth",
+    salary: 800,
+   });
+
+colombia.push({
+   name: "Liliana",
+    salary: 670,
+   });
+
+colombia.push({
+    name: "Karla",
+    salary: 2800,
+   });
+
+colombia.push({
+    name: "Ana",
+    salary: 3000,
+   });
+
+colombia.push({
+   name: "Jairo",
+    salary: 1300,
+   });
+
+colombia.push({
+    name: "Teresa",
+    salary: 2300,
+   });
+
+colombia.push({
+    name: "Julieth",
+    salary: 3300,
+   });
+
+colombia.push({
+   name: "Edwin",
+    salary: 1850,
+   });
+
+colombia.push({
+    name: "Guillermo",
+    salary: 2400,
+   });
+
+colombia.push({
+    name: "Monica",
+    salary: 1100,
+   });
+
+colombia.push({
+   name: "Isabela",
+    salary: 1200,
+   });
+
+colombia.push({
+    name: "Antonella",
+    salary: 2200,
+   });
+
+colombia.push({
+    name: "Pablo",
+    salary: 1500,
+   });
+
+colombia.push({
+   name: "Cristian",
+    salary: 4000,
+   });
+
+colombia.push({
+    name: "Andy",
+    salary: 2400,
+   });
+
+colombia.push({
+    name: "Oscar",
+    salary: 1500,
+   });
+
+colombia.push({
+   name: "Renata",
+    salary: 3900,
+   });
+
+colombia.push({
+    name: "Ruby",
+    salary: 3800,
+   });
